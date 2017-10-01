17049a98d04a5e8765d695480cbbab6b6af531f4
Erik Gilling <konkers@google.com>
2017-09-30 18:31:04 -0700

    Add config file support.

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"encoding/binary"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"regexp"
 	"strconv"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/asdine/storm"
 	"github.com/bwmarrin/discordgo"
@@ -99,7 +97,7 @@ func creditsCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []str
 }
 
 func init() {
-	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.StringVar(&configFilename, "config", "teletran.json", "Config file.")
 	flag.Parse()
 
 	MainCommandSet.AddCommand("echo", "echo echo echo echo", echoCommand, false)
@@ -113,23 +111,27 @@ var admins = map[string]bool{
 	"337056040703229955": true, // konkers
 }
 
-var token string
-var buffer = make([][]byte, 0)
-var commandPrefix = "!"
+var configFilename string
+var config Config
 var argsRe = regexp.MustCompile(`\s+`)
+
+type Config struct {
+	Token         string `json:"token"`
+	ClientId      string `json:"client_id"`
+	CommandPrefix string `json:"command_prefix"`
+}
 
 func main() {
 
-	if token == "" {
-		fmt.Println("No token provided. Please run: airhorn -t <bot token>")
+	b, err := ioutil.ReadFile(configFilename)
+	if err != nil {
+		fmt.Printf("Error reading from %s: %s\n", configFilename, err.Error())
 		return
 	}
 
-	// Load the sound file.
-	err := loadSound()
+	err = json.Unmarshal(b, &config)
 	if err != nil {
-		fmt.Println("Error loading sound: ", err)
-		fmt.Println("Please copy $GOPATH/src/github.com/bwmarrin/examples/airhorn/airhorn.dca to this directory.")
+		fmt.Printf("Error decoding config: %s\n", err.Error())
 		return
 	}
 
@@ -140,7 +142,7 @@ func main() {
 	}
 
 	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + token)
+	dg, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
 		fmt.Println("Error creating Discord session: ", err)
 		return
@@ -162,9 +164,9 @@ func main() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Airhorn is now running.  Press CTRL-C to exit.")
-	fmt.Printf("https://discordapp.com/oauth2/authorize?client_id=%d&scope=bot,&permissions=%d",
-		363395462608191500,
+	fmt.Println("Teletran1 is now running.  Press CTRL-C to exit.")
+	fmt.Printf("https://discordapp.com/oauth2/authorize?client_id=%s&scope=bot&permissions=%d\n",
+		config.ClientId,
 		discordgo.PermissionAllText|discordgo.PermissionAllVoice)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
@@ -179,49 +181,20 @@ func main() {
 func ready(s *discordgo.Session, event *discordgo.Ready) {
 
 	// Set the playing status.
-	s.UpdateStatus(0, "!airhorn")
+	s.UpdateStatus(0, config.CommandPrefix+"help")
 }
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// Find the channel that the message came from.
-	c, err := s.State.Channel(m.ChannelID)
-	if err != nil {
-		// Could not find channel.
-		return
-	}
-
-	// Find the guild for that channel.
-	g, err := s.State.Guild(c.GuildID)
-	if err != nil {
-		// Could not find guild.
-		return
-	}
-
-	// check if the message is "!airhorn"
-	if strings.HasPrefix(m.Content, "!airhorn") {
-
-		// Look for the message sender in that guild's current voice states.
-		for _, vs := range g.VoiceStates {
-			if vs.UserID == m.Author.ID {
-				err = playSound(s, g.ID, vs.ChannelID)
-				if err != nil {
-					fmt.Println("Error playing sound:", err)
-				}
-
-				return
-			}
-		}
-	} else if strings.HasPrefix(m.Content, commandPrefix) {
-		args := argsRe.Split(strings.TrimPrefix(m.Content, commandPrefix), -1)
+	if strings.HasPrefix(m.Content, config.CommandPrefix) {
+		args := argsRe.Split(strings.TrimPrefix(m.Content, config.CommandPrefix), -1)
 		fmt.Printf("%q\n", args)
 		MainCommandSet.Exec(s, m, args)
 	}
@@ -230,91 +203,14 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 // This function will be called (due to AddHandler above) every time a new
 // guild is joined.
 func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-
 	if event.Guild.Unavailable {
 		return
 	}
 
 	for _, channel := range event.Guild.Channels {
 		if channel.ID == event.Guild.ID {
-			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			_, _ = s.ChannelMessageSend(channel.ID, "Teletran1 here awaiting orders.")
 			return
 		}
 	}
-}
-
-// loadSound attempts to load an encoded sound file from disk.
-func loadSound() error {
-
-	file, err := os.Open("airhorn.dca")
-	if err != nil {
-		fmt.Println("Error opening dca file :", err)
-		return err
-	}
-
-	var opuslen int16
-
-	for {
-		// Read opus frame length from dca file.
-		err = binary.Read(file, binary.LittleEndian, &opuslen)
-
-		// If this is the end of the file, just return.
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			err := file.Close()
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-
-		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
-			return err
-		}
-
-		// Read encoded pcm from dca file.
-		InBuf := make([]byte, opuslen)
-		err = binary.Read(file, binary.LittleEndian, &InBuf)
-
-		// Should not be any end of file errors
-		if err != nil {
-			fmt.Println("Error reading from dca file :", err)
-			return err
-		}
-
-		// Append encoded pcm data to the buffer.
-		buffer = append(buffer, InBuf)
-	}
-}
-
-// playSound plays the current buffer to the provided channel.
-func playSound(s *discordgo.Session, guildID, channelID string) (err error) {
-
-	// Join the provided voice channel.
-	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
-	if err != nil {
-		return err
-	}
-
-	// Sleep for a specified amount of time before playing the sound
-	time.Sleep(250 * time.Millisecond)
-
-	// Start speaking.
-	vc.Speaking(true)
-
-	// Send the buffer data.
-	for _, buff := range buffer {
-		vc.OpusSend <- buff
-	}
-
-	// Stop speaking
-	vc.Speaking(false)
-
-	// Sleep for a specificed amount of time before ending.
-	time.Sleep(250 * time.Millisecond)
-
-	// Disconnect from the provided voice channel.
-	vc.Disconnect()
-
-	return nil
 }
